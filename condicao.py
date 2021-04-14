89affbbc874d29d839b55265eb31956d348aac81
João Pedro <jp.soares.monte@gmail.com>
2021-04-13 22:57:07 -0300

    Modificações

--- condicao.py
+++ condicao.py
@@ -1,9 +1,12 @@
+#Retorna valor booleano dá condição descrita na questão
 def condicao(c):
     return ord(c) >= ord('0') and ord(c)<=ord('9')
 
+#Retorna a negação da função "condicao"
 def nega(c):
     return not condicao(c)
 
+#função principal
 def main():
     c = input('Digite algo: ')
     print(f'É condição? {condicao(c)}', f'Não é condição? {nega(c)}', sep='.\n', end='.')
