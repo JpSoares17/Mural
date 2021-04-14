14168b04997db884a4e405823d99e7502132f126
João Pedro <jp.soares.monte@gmail.com>
2021-04-14 18:19:56 -0300

    Modificação .py

--- simbvogconnum.py
+++ simbvogconnum.py
@@ -1,29 +1,32 @@
 def letra(c):
-    return ord(c) >= ord('A') and ord(c) <= ord('Z')
+    return ord(c) >= ord('A') and ord(c)<=ord('Z')
 
 def num(c):
     return ord(c)>=ord('0') and ord(c)<=ord('9')
 
-def vog(c):
-    return c in 'AEIOU'
+def vogal(c):
+    if letra(c) and c in 'AEIOU':
+        return c in 'AEIOU'
 
 def con(c):
-    if letra(c) and not(vog(c)):
+    if letra(c) and not vogal(c):
         return letra(c)
-    elif not letra(c):
-        return letra(c)
+    elif letra(c) and vogal(c):
+        return not vogal(c)
     else:
-        return not(vog(c))
+        return letra(c)
 
-def sim(c):
-    if not(letra(c)) and not(num(c)):
-        return not(letra(c))
-    else:
-        return not(letra(c)) and not(num(c))
 
 def main():
     c = input('Digite um caracter: ').upper()
-    print(f'É vogal: {vog(c)}', f'É consoante: {con(c)}', f'É número: {num(c)}', f'É símbolo: {sim(c)}', sep='.\n', end='.')
+    if num(c):
+        print('Número')
+    elif vogal(c):
+        print('Vogal')
+    elif con(c):
+        print('Consoante')
+    else:
+        print('Símbolo')
 
 if __name__ == '__main__':
     main()
