1cc484802150e1338708a547ab1fa90e7ae46bb9
João Pedro <jp.soares.monte@gmail.com>
2021-04-15 22:42:03 -0300

    Modificação .py

--- imc.py
+++ imc.py
@@ -1,7 +1,9 @@
 def imc(alt, mas):
     return mas/(alt**2)
 
-def cnd(alt, mas):
+def main():
+    mas = float(input(''))
+    alt = float(input(''))
     if imc(alt, mas)<18.5:
         print(f'{imc(alt, mas)}\nAbaixo do peso')
     elif 18.5 <= imc(alt, mas) < 25:
@@ -14,11 +16,6 @@ def cnd(alt, mas):
         print(f'{imc(alt, mas)}\nObeso moderado')
     elif 40 <= imc(alt, mas):
         print(f'{imc(alt, mas)}\nObeso mórbido')
-
-def main():
-    mas = float(input(''))
-    alt = float(input(''))
-    cnd(alt, mas)
 
 if __name__ == '__main__':
     main()
