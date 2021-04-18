4d1424acee497a53a161786e6ee72afb5c5bac56
João Pedro <jp.soares.monte@gmail.com>
2021-04-18 19:16:49 -0300

    Arquivo Novo .py

---
+++ crescente.py
@@ -0,0 +1,11 @@
+def main():
+    n1 = int(input(''))
+    n2 = int(input(''))
+    n3 = int(input(''))
+    x = [n1, n2, n3]
+    x.sort()
+    for i in x:
+        print(f'{i}')
+
+if __name__ == '__main__':
+    main()
