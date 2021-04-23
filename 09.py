c8c488a3ba664a418ca1d3a2d026d01075566b08
João Pedro <jp.soares.monte@gmail.com>
2021-04-23 00:23:42 -0300

    Arquivo Novo

---
+++ 09.py
@@ -0,0 +1,13 @@
+def condicao(c):
+    if ord('0')<=ord(c)<=ord('9'):
+        return ord('0')<=ord(c)<=ord('9')
+    else:
+        return ord('0')<=ord(c)<=ord('9')
+
+def main():
+    c = input('')
+    d=condicao(c)
+    print(d)
+
+if __name__ == '__main__':
+    main()
