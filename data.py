24ad78dedf8af8d6e9be99b07633e9e103b3484e
João Pedro <jp.soares.monte@gmail.com>
2021-04-22 23:58:18 -0300

    Arquivo Novo

---
+++ data.py
@@ -0,0 +1,11 @@
+def data(d, m, a):
+    return f'{d}/{m}/{a}'
+
+def main():
+    d = int(input(''))
+    m = int(input(''))
+    a = int(input(''))
+    print(data(d, m, a))
+
+if __name__ == '__main__':
+    main()
