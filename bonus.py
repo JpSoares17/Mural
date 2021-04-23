437d239f26332b89a73501e53d489a093f0ac040
João Pedro <jp.soares.monte@gmail.com>
2021-04-22 23:52:29 -0300

    Arquivo Novo .py

---
+++ bonus.py
@@ -0,0 +1,12 @@
+def bonus(a, b):
+    c = a*b
+    return c
+
+def main():
+    anos = int(input(''))
+    sal = float(input(''))
+    bon = bonus(anos, sal)
+    print(bon)
+
+if __name__ == '__main__':
+    main()
