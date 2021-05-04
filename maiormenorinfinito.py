8f99a9b71e40be9f845f06ba61bb3d736246bcf0
João Pedro <jp.soares.monte@gmail.com>
2021-05-04 13:59:49 -0300

    Arquivo Novo

--- maiormenorinfinito.py
+++ maiormenorinfinito.py
@@ -1,18 +1,24 @@
-def main():
-    ma=0
-    x=[]
-    q=0
-    while True:
-        n=int(input(''))
-        x.append(n)
-        if n>ma:
-            ma=n
-        q+=1
-        if n==0: break
-    del(x[q-1])
-    if ma==0 or min(x)==0:
+def sai(ma, me):
+    if ma==0 or me==0:
         pass
     else:
-        print(f'{ma}\n{min(x)}')
+        return f'{ma}\n{me}'
+
+def process(n, ma, me):
+    while True:
+        nc = int(input(''))
+        if nc>n and nc!=0:
+            ma=nc
+        if nc<n and nc!=0:
+            me=nc
+        if nc==0: break
+    return ma, me
+
+def main():
+    n = int(input(''))
+    ma, me = n, n
+    ma, me = process(n, ma, me)
+    print(sai(ma, me))
+
 if __name__ == '__main__':
     main()
