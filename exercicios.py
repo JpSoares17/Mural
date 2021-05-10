30ea3a26b13f0fb9050fa01f47ba40515e9e7244
João Pedro <jp.soares.monte@gmail.com>
2021-05-09 22:09:04 -0300

    Arquivo Novo

--- exercicios.py
+++ exercicios.py
@@ -1,2 +1,16 @@
-for i in range(0, 10, 3):
-    print(i)
+def main():
+    s, d = float(input('')), float(input(''))
+    m = 10
+    a = 2016
+    while True:
+        m+=1
+        d=1.15*d
+        if m>12:
+            m=1
+            a+=1
+        if m == 3:
+            s= 1.05*s
+        if d>s: break
+    print(f'{m}/{a}')
+if __name__ == '__main__':
+    main()
