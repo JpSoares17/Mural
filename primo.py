b027a31b536d24357b92b93cb08dbaab7cbe2aba
João Pedro <jp.soares.monte@gmail.com>
2021-05-13 23:34:29 -0300

    Arquivo Novo

---
+++ primo.py
@@ -0,0 +1,17 @@
+def proc(n):
+    q=0
+    np=0
+    for i in range(n-1, 1, -1):
+        q+=1
+        if n%i==0 and n!=2:
+            np+=1
+    if np==0:
+        return np==0
+    else:
+        return np==0
+
+def main():
+    n=int(input(''))
+    print(proc(n))
+if __name__ == '__main__':
+    main()
