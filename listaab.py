eba564183fb68a5b39d92c92679c3894f0752239
João Pedro <jp.soares.monte@gmail.com>
2021-05-26 11:18:18 -0300

    Arquivo Novo

---
+++ listaab.py
@@ -0,0 +1,22 @@
+def intercalar(lista_1, lista_2):
+    lista_3 = [*sum(zip(lista_1, lista_2), ())]
+    return lista_3
+
+def main():
+    lista_a = []
+    lista_b = []
+    for i in range(25):
+        #Leitura de A:
+        elementos = int(input().strip())
+        lista_a.append(elementos)
+    for j in range(25):
+        #Leitura de B
+        elements = int(input().strip())
+        lista_b.append(elements)
+    #Processamento:
+    lista_c = intercalar(lista_a, lista_b)
+
+    #Saída:
+    print(lista_a, lista_b, lista_c, sep='\n')
+if __name__ == '__main__':
+    main()
