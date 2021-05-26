5f496cb2590cc257a7e6bdad997d172c3023fa61
João Pedro <jp.soares.monte@gmail.com>
2021-05-26 11:39:25 -0300

    Arquivo Novo

--- listaab.py
+++ listaab.py
@@ -7,11 +7,11 @@ def main():
     lista_b = []
     for i in range(25):
         #Leitura de A:
-        elementos = int(input().strip())
+        elementos = int(input('Digite números: ').strip())
         lista_a.append(elementos)
     for j in range(25):
         #Leitura de B
-        elements = int(input().strip())
+        elements = int(input('Digite números: ').strip())
         lista_b.append(elements)
     #Processamento:
     lista_c = intercalar(lista_a, lista_b)
