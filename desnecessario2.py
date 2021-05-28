2f8068d334eae6b5cb4e2fa54f55e25b90e30b47
João Pedro <jp.soares.monte@gmail.com>
2021-05-28 01:16:05 -0300

    Arquivo Novo

---
+++ desnecessario2.py
@@ -0,0 +1,23 @@
+def contador(lista, numero_contado):
+    vezes_repetidas = 0
+    for i in lista:
+        if i==numero_contado:
+            vezes_repetidas+=1
+    return vezes_repetidas
+
+def main():
+    #Leitura:
+    lista_leitura = []
+    while True:
+        itens = int(input(''))
+        if itens == 0: break
+        lista_leitura.append(itens)
+    item_contagem = int(input(''))
+
+    #Processamento:
+    quantidades_repetidas = contador(lista_leitura, item_contagem)
+
+    #Saída:
+    print(lista_leitura, item_contagem, quantidades_repetidas, sep='\n')
+if __name__ == '__main__':
+    main()
