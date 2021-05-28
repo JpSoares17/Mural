bf7a87f4fcdc65f59c8e62f39faeacaefd53d4a2
João Pedro <jp.soares.monte@gmail.com>
2021-05-28 09:38:09 -0300

    Arquivo Novo

---
+++ somalista.py
@@ -0,0 +1,23 @@
+def soma_lista(lista_1, lista_2):
+    lista_3 = zip(lista_1, lista_2)
+    soma = [item_1+item_2 for (item_1, item_2) in lista_3]
+    return soma
+
+def main():
+    #Leitura:
+    lista_a = []
+    lista_b = []
+    for i in range(20):
+        itens_a = int(input(''))
+        lista_a.append(itens_a)
+    for j in range(20):
+        itens_b = int(input(''))
+        lista_b.append(itens_b)
+
+    #Processamento:
+    lista_c = soma_lista(lista_a, lista_b)
+
+    #Saída:
+    print(lista_a, lista_b, lista_c, sep='\n')
+if __name__ == '__main__':
+    main()
